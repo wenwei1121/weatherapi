e98fcc2c7802c705a7c34da96fddc59cad97fefb
Alweis_Do <wenweiyang1121@gmail.com>
2022-03-05 10:54:04 +0800

    weatherapi-v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-const purgecss = require('gulp-purgecss')
+// const purgecss = require('gulp-purgecss')
 
 function bulidStyles() {
   return src('scss/**/*.scss')
     .pipe(sass())
-    .pipe(purgecss({content: ['*.html'],
-      safelist: {
-        standard: [/top$/, /bottom$/]
-      }
-    }))
+    // .pipe(purgecss({content: ['*.html'],
+    //   safelist: {
+    //     standard: [/top$/, /bottom$/]
+    //   }
+    // }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['scss/**/*.scss', '*.html'], bulidStyles)
+  watch(['scss/**/*.scss'], bulidStyles)
 }
 
 exports.default = series(bulidStyles, watchTask)
